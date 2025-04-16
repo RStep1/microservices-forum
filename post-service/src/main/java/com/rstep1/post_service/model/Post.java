6e1771fd6c1aba65d9db13dd895add60aeecd221
RStep1 <stepanrykov.itmo@gmail.com>
2025-04-16 23:32:13 +0300

    feat: create post entity

---
+++ post-service/src/main/java/com/rstep1/post_service/model/Post.java
@@ -0,0 +1,56 @@
+package com.rstep1.post_service.model;
+
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "posts")
+public class Post {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false, updatable = false)
+    private Long authorId;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    
+    private LocalDateTime updatedAt;
+    
+    @Column(length = 150, nullable = false)
+    private String title;
+
+    @Lob
+    private String content;
+
+    @Column(nullable = false)
+    private Long voteCount;
+
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.voteCount = 0L;
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
